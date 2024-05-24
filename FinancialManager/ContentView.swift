38f53960f18bc0c4be9bd8120c53fa057f6e0937
serabraa <sergey.abraham7@gmail.com>
2024-05-24 15:56:20 +0400

    Initial commit

---
+++ FinancialManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinancialManager
+//
+//  Created by Sergey on 24.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hell, noo, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinancialManager/FinancialManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialManagerApp.swift
+//  FinancialManager
+//
+//  Created by Sergey on 24.05.24.
+//
+
+import SwiftUI
+
+@main
+struct FinancialManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
