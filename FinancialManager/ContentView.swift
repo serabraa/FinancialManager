108fcf18bcf8c7bd0c885fb7c8d021ba953c5612
serabraa <sergey.abraham7@gmail.com>
2024-06-11 16:39:48 +0400

    First, good date with right and left buttons
    nextday(),previousDay() functions and working ui

--- FinancialManager/ContentView.swift
+++ FinancialManager/ContentView.swift
@@ -8,17 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentDate: Date = Date()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hell, noo, world!")
+            HStack {
+                Button(action: {
+                    previousDay()
+                }) {
+                    Image(systemName: "chevron.left")
+                        .padding()
+                }
+                
+                Spacer()
+                
+                Text(currentDate.formatted(date: .complete, time: .omitted))
+                    .font(.title)
+                    .padding()
+//                    .gesture(
+//                                            DragGesture(minimumDistance: 20)
+//                                                .onEnded { swipe in
+//                                                    if swipe.translation.width > 0 {
+//                                                        previousDay()
+//                                                    } else if swipe.translation.width < 0 {
+//                                                        nextDay()
+//                                                    }
+//                                                }
+//                                        )
+
+                Spacer()
+                
+                Button(action: {
+                    nextDay()
+                }) {
+                    Image(systemName: "chevron.right")
+                        .padding()
+                }
+            }
         }
-        .padding()
+    }
+    
+    func nextDay() {
+        currentDate = Calendar.current.date(byAdding: .day, value: 1, to: currentDate)!
+    }
+
+    func previousDay() {
+        currentDate = Calendar.current.date(byAdding: .day, value: -1, to: currentDate)!
     }
 }
 
 #Preview {
     ContentView()
 }
+
